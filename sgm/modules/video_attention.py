a999143eb40719dfaed1ca2c98d0448449cea8ad
nina23-ctrl <christina.zachariou.2323@gmail.com>
2026-01-25 08:20:14 +0200

    Update video_attention.py

--- sgm/modules/video_attention.py
+++ sgm/modules/video_attention.py
@@ -32,6 +32,7 @@ class VideoTransformerBlock(nn.Module):
         ff_in=False,
         inner_dim=None,
         disable_self_attn=False,
+        attn_type=attn_mode,
         disable_temporal_crossattention=False,
         switch_temporal_ca_to_sa=False,
     ):
